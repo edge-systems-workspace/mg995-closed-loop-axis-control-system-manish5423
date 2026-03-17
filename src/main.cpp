b5513dc71dd9ee5a6aa79996ebb8417702c31a80
Manish Saini <manishsainigvp12@gmail.com>
2026-03-17 09:01:33 +0530

    initialize servo control and setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
+#define SERVO_PIN 9
 void setup() {
-// write your initialization code here
+    myServo.attach(SERVO_PIN);
+    Serial.begin(9600);
 }
 
-
+/*
 void loop() {
-// write your code here
-}
+    /*myServo.write(0);
+    delay(1000);
+    myServo.write(90);
+    delay(1000);
+    myServo.write(120);
+    delay(1000);
+    myServo.write(180);
+    delay(1000);*/
+
+
+ /*   for(int i = 0; i <= 180; i++) {
+        myServo.write(i);
+        delay(500);
+    }
+    for(int i = 180; i >= 0; i--) {
+        myServo.write(i);
+        delay(500);
+    }
+} */
